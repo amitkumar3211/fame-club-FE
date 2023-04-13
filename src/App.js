e21b0db36b768dd63bd32a46e925e16f6c2636c8
amitkumar3211 <amitrajput3223@gmail.com>
2023-04-13 22:26:42 +0530

    completed register page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './index.css';
 import Footer from './components/footer/footer';
 import Header from './components/header/Header';
-import { Home } from './components/home/home';
-
+import { Home } from './components/home/Home';
+import Register from './components/signup/register';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<Register />} />
       </Routes>
     </BrowserRouter>
     <Footer />
